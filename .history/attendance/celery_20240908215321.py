4c3ca893b394cb9df31727bb2e73a89dd0a54b17
vijay gholve <vijaygholve77c@gmail.com>
2024-09-11 00:24:39 +0530

    UP

---
+++ .history/attendance/celery_20240908215321.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+from django.conf import settings
+from celery.schedules import crontab  
+from django_celery_beat.models import CrontabSchedule, PeriodicTask
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'attendance.settings')
+
+app = Celery('attendance')
+app.conf.enable_utc = False
+app.conf.update(timezone='Asia/Kolkata')
+app.config_from_object(settings, namespace='CELERY')
+app.autodiscover_tasks()
+                                             
+
+# app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# # Load task modules from all registered Django app configs.
+# app.autodiscover_tasks()
+@app.task(bind=True)
+def bind_fun(self):
+    print(f"Request: {self.request!r}")
---
+++ .history/attendance/celery_20240908224555.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+from django.conf import settings
+from celery.schedules import crontab
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'attendance.settings')
+
+app = Celery('attendance')
+
+# Timezone settings
+app.conf.enable_utc = False
+app.conf.update(timezone='Asia/Kolkata')
+
+# Load task settings from Django settings with CELERY_ prefix
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Auto-discover tasks in installed apps
+app.autodiscover_tasks()
+
+# Celery beat schedule
+app.conf.beat_schedule = {
+    'print-hello': {
+        'task': 'students.task.send_absence_emails',#C:\Users\Vijay\python pro\attendance2\students\task.py
+        'schedule': crontab(hour=17, minute=32),
+        },
+}
+
+@app.task(bind=True)
+def bind_fun(self):
+    print(f"Request: {self.request!r}")
